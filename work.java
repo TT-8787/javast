27ef792299352eec54950988430a6d2230882a3d
TT-8787 <852068787@QQ.COM>
2024-07-02 19:46:03 +0800

    Add files via upload

---
+++ work.java
@@ -0,0 +1,10 @@
+public class work {
+    public static void main(String[] args ){
+        String name = "king";
+        int age = 20;
+        double score = 20.5;
+        char sex = '男';
+        String hobby = "打篮球";
+        System.out.println("姓名\t年龄\t成绩\t性别\t爱好\n"+name+"\t"+age+"\t"+score+"\t"+sex+"\t"+hobby);
+    }
+}
