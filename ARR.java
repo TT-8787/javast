26bf122eab98e828dc2c6c94096886fda2ad51e4
TT-8787 <852068787@QQ.COM>
2024-07-08 15:45:33 +0800

    Add files via upload

---
+++ ARR.java
@@ -0,0 +1,9 @@
+public class work {
+    public static void main(String[] args){
+        char[] chars = new char[26];
+        for(int i=0;i<chars.length;i++){
+            chars[i] = (char)('A'+i);
+            System.out.println(chars[i]+" ");
+        }
+    }
+}
---
+++ ARR2.java
@@ -0,0 +1,14 @@
+public class work2 {
+    public static void main(String[] args){
+        int[] arr={4,-1,9,10,23};
+        int max = arr[0];
+        int maxIndex = 0;
+        for(int i = 0;i<arr.length;i++){
+            if (max < arr[i]){
+                max=arr[i];
+                maxIndex=i;
+            }
+        }
+        System.out.println("最大值："+max+",对应下标是："+maxIndex);
+    }
+}
